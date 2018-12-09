f0b037bf716e0c1230051ec632adc0d070af2568
Daniel Whiffing <danielwhiffing@gmail.com>
2018-12-09 10:57:40 -0500

    Improves styling of nav, aside and p tags

--- code/aside.js
+++ code/aside.js
@@ -2,6 +2,8 @@ import React, { Fragment } from "react";
 
 const prefix = process.env.NODE_ENV === "production" ? "/fury" : "";
 
+const regex = /(Air Force|Navy|Army)/;
+
 const Aside = ({ nav, pages, name, country }) => {
   const keys = Object.keys(nav.index[name]);
   const doThing = key => {
@@ -16,11 +18,15 @@ const Aside = ({ nav, pages, name, country }) => {
 
         {shouldRenderDeepLinks && (
           <ul>
-            {Object.keys(countryNavData).map(key => (
-              <li>
-                <a href={`${prefix}/${key}`}>{pages[key].label}</a>
-              </li>
-            ))}
+            {Object.keys(countryNavData).map(key => {
+              const match = pages[key].label.match(regex);
+              const label = match ? match[0] : pages[key].label;
+              return (
+                <li>
+                  <a href={`${prefix}/${key}`}>{label}</a>
+                </li>
+              );
+            })}
           </ul>
         )}
       </li>
--- code/nav.js
+++ code/nav.js
@@ -3,47 +3,60 @@ import React, { Fragment } from "react";
 const prefix = process.env.NODE_ENV === "production" ? "/fury" : "";
 
 const Nav = ({ _nav, tabs }) => (
-  <ul>
-    <a href="/">
-      <img className="logo" src={`${prefix}/assets/images/logo.png`} />
-    </a>
-    {Object.keys(tabs).map(tabKey => {
-      const tab = tabs[tabKey];
-      let path = _nav.index[tabKey] || "";
-      let pathName = typeof path === "string" ? path : tabKey;
+  <div className="nav-container">
+    <div className="mobile-container">
+      <a href="/">
+        <img className="logo" src={`${prefix}/assets/images/logo.png`} />
+      </a>
+    </div>
+    <ul>
+      <li className={`nav-top-button tab-home`}>
+        <a href="/">
+          <img className="logo" src={`${prefix}/assets/images/logo.png`} />
+        </a>
+      </li>
+      <div className="tab-container">
+        {Object.keys(tabs).map(tabKey => {
+          const tab = tabs[tabKey];
+          let path = _nav.index[tabKey] || "";
+          let pathName = typeof path === "string" ? path : tabKey;
 
-      return (
-        <li className={`tab-${pathName}`}>
-          <a href={`${prefix}/${pathName}`}>{tab.label}</a>
-          {typeof path === "object" && (
-            <ul className="dropdown">
-              {Object.keys(tab.items).map(itemKey => {
-                const item = tab.items[itemKey];
-                const mainHref = `${prefix}/${pathName}/${item.path}`;
-                return (
-                  <li>
-                    <a href={mainHref}>{item.label}</a>
-                    {tab.label !== "Scenarios" && (
-                      <ul className="dropdown">
-                        {["Air Force", "Army", "Navy"].map(label => {
-                          const subPath = label.toLowerCase().replace(" ", "_");
-                          return (
-                            <li>
-                              <a href={`${mainHref}/${subPath}`}>{label}</a>
-                            </li>
-                          );
-                        })}
-                      </ul>
-                    )}
-                  </li>
-                );
-              })}
-            </ul>
-          )}
-        </li>
-      );
-    })}
-  </ul>
+          return (
+            <li className={`nav-top-button tab-${pathName}`}>
+              <a href={`${prefix}/${pathName}`}>{tab.label}</a>
+              {typeof path === "object" && (
+                <ul className="dropdown">
+                  {Object.keys(tab.items).map(itemKey => {
+                    const item = tab.items[itemKey];
+                    const mainHref = `${prefix}/${pathName}/${item.path}`;
+                    return (
+                      <li>
+                        <a href={mainHref}>{item.label}</a>
+                        {tab.label !== "Scenarios" && (
+                          <ul className="dropdown">
+                            {["Air Force", "Army", "Navy"].map(label => {
+                              const subPath = label
+                                .toLowerCase()
+                                .replace(" ", "_");
+                              return (
+                                <li>
+                                  <a href={`${mainHref}/${subPath}`}>{label}</a>
+                                </li>
+                              );
+                            })}
+                          </ul>
+                        )}
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
+            </li>
+          );
+        })}
+      </div>
+    </ul>
+  </div>
 );
 
 export default Nav;
