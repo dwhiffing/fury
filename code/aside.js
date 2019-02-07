4245fb9bc5d7c10409e22aa54a2d151615de6d1f
Daniel Whiffing <danielwhiffing@gmail.com>
2019-02-07 15:19:17 -0500

    Sorts countries alphabetically in navigation

--- code/aside.js
+++ code/aside.js
@@ -1,39 +1,45 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react'
 
-const prefix = process.env.NODE_ENV === "production" ? "/fury" : "";
+const prefix = process.env.NODE_ENV === 'production' ? '/fury' : ''
 
-const regex = /(Air Force|Navy|Army)/;
+const regex = /(Air Force|Navy|Army)/
 
 String.prototype.capitalize = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1);
-};
+  return this.charAt(0).toUpperCase() + this.slice(1)
+}
 
 const Aside = ({ nav, pages, name, country, section }) => {
-  const keys = Object.keys(nav.index[name]);
+  const keys = Object.keys(nav.index[name])
+
+  const sortedKeys = Object.keys(nav.index[name]).sort((a, b) => {
+    const pageALabel = pages[a].label.replace('The ', '')
+    const pageBLabel = pages[b].label.replace('The ', '')
+    return pageALabel[0] > pageBLabel[0] ? 1 : -1
+  })
 
   const renderDeepLinks = data => {
     return (
       <ul>
         {Object.keys(data).map(key => {
-          const navData = data[key];
-          const page = pages[key];
-          const pathArray = key.split("/");
+          const navData = data[key]
+          const page = pages[key]
+          const pathArray = key.split('/')
           const fallbackLabel = pathArray[pathArray.length - 1]
-            .replace("-", " ")
-            .capitalize();
+            .replace('-', ' ')
+            .capitalize()
 
-          const pageLabel = page.label || fallbackLabel;
-          const match = pageLabel.match(regex);
-          const label = match ? match[0] : pageLabel;
+          const pageLabel = page.label || fallbackLabel
+          const match = pageLabel.match(regex)
+          const label = match ? match[0] : pageLabel
 
-          const _section = key.split("/")[3];
+          const _section = key.split('/')[3]
 
-          if (typeof navData === "string") {
+          if (typeof navData === 'string') {
             return (
               <li>
                 <a href={`${prefix}/${key}`}>{label}</a>
               </li>
-            );
+            )
           } else {
             return (
               <Fragment>
@@ -42,18 +48,18 @@ const Aside = ({ nav, pages, name, country, section }) => {
                 </li>
                 {(match || _section === section) && renderDeepLinks(navData)}
               </Fragment>
-            );
+            )
           }
         })}
       </ul>
-    );
-  };
+    )
+  }
 
   const renderLinks = key => {
-    const countryNavData = nav.index[name][key];
-    const page = pages[key];
+    const countryNavData = nav.index[name][key]
+    const page = pages[key]
     const shouldRenderDeepLinks =
-      typeof countryNavData === "object" && key.split("/")[1] === country;
+      typeof countryNavData === 'object' && key.split('/')[1] === country
 
     return (
       <li>
@@ -61,15 +67,19 @@ const Aside = ({ nav, pages, name, country, section }) => {
 
         {shouldRenderDeepLinks && renderDeepLinks(countryNavData)}
       </li>
-    );
-  };
+    )
+  }
 
   return (
     <Fragment>
-      <ul>{keys.slice(0, keys.length / 2).map(renderLinks)}</ul>
-      <ul>{keys.slice(keys.length / 2, keys.length).map(renderLinks)}</ul>
+      <ul>{sortedKeys.slice(0, sortedKeys.length / 2).map(renderLinks)}</ul>
+      <ul>
+        {sortedKeys
+          .slice(sortedKeys.length / 2, sortedKeys.length)
+          .map(renderLinks)}
+      </ul>
     </Fragment>
-  );
-};
+  )
+}
 
-export default Aside;
+export default Aside
--- code/nav.js
+++ code/nav.js
@@ -1,58 +1,57 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react'
 
-const prefix = process.env.NODE_ENV === "production" ? "/fury" : "";
+const prefix = process.env.NODE_ENV === 'production' ? '/fury' : ''
 
 const Nav = ({ _nav, tabs }) => (
   <div className="nav-container">
     <div className="mobile-container">
-      <a href={`${process.env.NODE_ENV === "production" ? "/fury" : "/"}`}>
+      <a href={`${process.env.NODE_ENV === 'production' ? '/fury' : '/'}`}>
         <img className="logo" src={`${prefix}/assets/images/logo.png`} />
       </a>
     </div>
     <ul>
       <li className={`nav-top-button tab-home`}>
-        <a href={`${process.env.NODE_ENV === "production" ? "/fury" : "/"}`}>
+        <a href={`${process.env.NODE_ENV === 'production' ? '/fury' : '/'}`}>
           <img className="logo" src={`${prefix}/assets/images/logo.png`} />
         </a>
       </li>
       <div className="tab-container">
         {Object.keys(tabs).map(tabKey => {
-          const tab = tabs[tabKey];
-          let path = _nav.index[tabKey] || "";
-          let pathName = typeof path === "string" ? path : tabKey;
+          const tab = tabs[tabKey]
+          let path = _nav.index[tabKey] || ''
+          let pathName = typeof path === 'string' ? path : tabKey
 
           return (
             <li className={`nav-top-button tab-${pathName}`}>
               <a href={`${prefix}/${pathName}`}>{tab.label}</a>
-              {typeof path === "object" && (
+              {typeof path === 'object' && (
                 <ul className="dropdown">
-                  {Object.keys(tab.items).map(itemKey => {
-                    const item = tab.items[itemKey];
-                    const mainHref = `${prefix}/${pathName}/${item.path}`;
+                  {tab.items.map(item => {
+                    const mainHref = `${prefix}/${pathName}/${item.path}`
                     return (
                       <li>
                         <a href={mainHref}>{item.label}</a>
                         <ul className="dropdown">
-                          {["Air Force", "Army", "Navy"].map(label => {
-                            const subPath = label.toLowerCase().split(" ")[0];
+                          {['Air Force', 'Army', 'Navy'].map(label => {
+                            const subPath = label.toLowerCase().split(' ')[0]
                             return (
                               <li>
                                 <a href={`${mainHref}/${subPath}`}>{label}</a>
                               </li>
-                            );
+                            )
                           })}
                         </ul>
                       </li>
-                    );
+                    )
                   })}
                 </ul>
               )}
             </li>
-          );
+          )
         })}
       </div>
     </ul>
   </div>
-);
+)
 
-export default Nav;
+export default Nav
