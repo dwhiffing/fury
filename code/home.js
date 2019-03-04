606905391413fd342763e44a75f351fc0b8d8877
Daniel Whiffing <dan@Daniels-MacBook-Pro.local>
2019-03-03 19:45:40 -0500

    Updates Home page

--- code/home.js
+++ code/home.js
@@ -2,12 +2,26 @@ import PropTypes from "prop-types";
 import React, { Fragment } from "react";
 
 const Home = ({ _body }) => (
-  <div style={{ maxWidth: 700, margin: '40px auto' }}>
-    {_body}
-    <p>More information on COMMAND Modern Air/Naval Operations can be found at:</p>
-    <a href="http://www.warfaresims.com/"><img src="/assets/images/warfaresims.gif" alt="Warfare Sims" /></a>
-    <a href="http://www.matrixgames.com/"><img src="/assets/images/matrix.png" alt="Matrix Games" /></a>
-    <br/>
+  <div>
+    <div className="hero" alt="Home">
+      <img className="hero_logo" src="/assets/images/home_logo.png" alt="Northern Fury" />
+    </div>
+
+    <div className="home-wrapper">
+      <div className="home-content">
+        {_body}
+      </div>
+
+      <div style={{ flex: '1' }}>
+        <img src="/assets/images/cover.jpg" alt="Cover" />
+        <p className="sponsors">More information on COMMAND Modern Air/Naval Operations can be found at:</p>
+
+        <div style={{ display: 'flex' }}>
+          <a href="http://www.warfaresims.com/"><img src="/assets/images/warfaresims.gif" alt="Warfare Sims" /></a>
+          <a href="http://www.matrixgames.com/"><img src="/assets/images/matrix.png" alt="Matrix Games" /></a>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
