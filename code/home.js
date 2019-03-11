60d7bb2bf2bb9c0515dec23314ea0068b62b90a2
Daniel Whiffing <dan@Daniels-MacBook-Pro.local>
2019-03-11 11:41:11 -0400

    Fixes issue with prefix and styling on mobile

--- code/home.js
+++ code/home.js
@@ -1,10 +1,12 @@
 import PropTypes from "prop-types";
 import React, { Fragment } from "react";
 
+const prefix = process.env.NODE_ENV === 'production' ? '/fury' : ''
+
 const Home = ({ _body }) => (
   <div>    
     <div className="hero" alt="Home">
-      <img className="hero_logo" src="/assets/images/home_logo.png" alt="Northern Fury" />
+      <img className="hero_logo" src={`${prefix}/assets/images/home_logo.png`} alt="Northern Fury" />
     </div>
 
     <div className="home-wrapper">
@@ -13,12 +15,12 @@ const Home = ({ _body }) => (
       </div>
 
       <div style={{ flex: '1', justifyContent: 'center' }}>
-        <img src="/assets/images/cover.jpg" alt="Cover" />
+        <img src={`${prefix}/assets/images/cover.jpg`} alt="Cover" />
         <p className="sponsors">More information on COMMAND Modern Air/Naval Operations can be found at:</p>
 
         <div style={{ display: 'flex' }}>
-          <a href="http://www.warfaresims.com/"><img src="/assets/images/warfaresims.gif" alt="Warfare Sims" /></a>
-          <a href="http://www.matrixgames.com/"><img src="/assets/images/matrix.png" alt="Matrix Games" /></a>
+          <a href="http://www.warfaresims.com/"><img src={`${prefix}/assets/images/warfaresims.gif`} alt="Warfare Sims" /></a>
+          <a href="http://www.matrixgames.com/"><img src={`${prefix}/assets/images/matrix.png`} alt="Matrix Games" /></a>
         </div>
       </div>
     </div>
