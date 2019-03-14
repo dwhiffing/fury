73cf235df177983c2fcbf28bd2b7832c287b5a4a
Daniel Whiffing <dan@Daniels-MacBook-Pro.local>
2019-03-13 22:35:46 -0400

    Fixes aside

--- code/aside.js
+++ code/aside.js
@@ -2,7 +2,12 @@ import React, { Fragment } from 'react'
 
 const prefix = process.env.NODE_ENV === 'production' ? '/fury' : ''
 
-const regex = /(Air Force|Navy|Army)/
+const sanitizeLabel = (label) => {
+  const match = label.match(/(Air Force|Navy|Army)/)
+  const sanitizedLabel = match ? match[0] : label
+
+  return sanitizedLabel.replace(/Naval Aviation |MEF |Amphibious /, '')
+}
 
 String.prototype.capitalize = function() {
   return this.charAt(0).toUpperCase() + this.slice(1)
@@ -11,48 +16,50 @@ String.prototype.capitalize = function() {
 const Aside = ({ nav, pages, pathArray }) => {
   const [name, currentCountry, currentSection] = pathArray
 
+  
   const renderDeepLinks = data => {
+    if (typeof data !== 'object') {
+      return null
+    }
+    
     let list = Object.keys(data)
 
     list = list.sort((a, b) => {
-      const pageAPosition = pages[a].position || -1
-      const pageBPosition = pages[b].position || -1
+      const pageAPosition = typeof pages[a].position === 'number' ? pages[a].position : 0
+      const pageBPosition = typeof pages[b].position === 'number' ? pages[b].position : 0
       return pageAPosition > pageBPosition ? 1 : -1
     })
 
     return (
       <ul>
         {list.map(key => {
+
           const navData = data[key]
-          const page = pages[key]
           const splitKey = key.split('/')
-          const renderedPage = splitKey[splitKey.length - 1]
-          const fallbackLabel = renderedPage
-            .replace('-', ' ')
-            .capitalize()
-
-          const pageLabel = page.label || page.title || fallbackLabel
-          const match = pageLabel.match(regex)
-          let label = match ? match[0] : pageLabel
-          const sanitizedLabel = label.replace(/MEF|Amphibious|Naval Aviation/g, '').trim()
-          if (sanitizedLabel !== '') {
-            label = sanitizedLabel
-          }
-
           const isActiveSection = splitKey[2] === currentSection && splitKey.length < 4
           const isActivePage = pathArray[pathArray.length - 1] === splitKey[splitKey.length - 1]
           const isActive = isActiveSection || isActivePage
-          const shouldRenderDeepLinks = splitKey.length < 4 || pathArray[3] === splitKey[3]
+          
+          const [_nat, pathCountry, pathGroup, pathGroup2] = pathArray
+          const [_nat1, renderedCountry, renderedGroup, renderedGroup2] = splitKey
+
+          let shouldRenderDeepLinks = pathCountry === renderedCountry
+
+          if (renderedGroup) {
+            shouldRenderDeepLinks = pathGroup === renderedGroup
+          }
+
+          if (renderedGroup2) {
+            shouldRenderDeepLinks = pathGroup2 === renderedGroup2
+          }
 
           const link = (
             <li key={`link-${key}`}>
               <a
                 href={`${prefix}/${key}`}
-                style={{
-                  fontWeight: isActive ? 'bold' : 'normal'
-                }}
+                style={{ fontWeight: isActive ? 'bold' : 'normal' }}
               >
-                {label} {splitKey.length > 2 && typeof navData === 'object' && '+'}
+                {sanitizeLabel(pages[key].label)} {splitKey.length > 2 && typeof navData === 'object' && '+'}
               </a>
             </li>
           )
@@ -63,8 +70,7 @@ const Aside = ({ nav, pages, pathArray }) => {
             return (
               <Fragment key={`link-${key}`}>
                 {link}
-                {/* need to fix MEF being weird due to extra nesting */}
-                {(isActive && shouldRenderDeepLinks || pathArray[4] === 'mef') && renderDeepLinks(navData)}
+                {shouldRenderDeepLinks && renderDeepLinks(navData)}
               </Fragment>
             )
           }
@@ -74,15 +80,12 @@ const Aside = ({ nav, pages, pathArray }) => {
   }
 
   const renderLinks = key => {
-    const countryNavData = nav.index[name][key]
-    const label = pages[key].label
     const isActive = key.split('/')[1] === currentCountry
 
     return (
       <li key={`link-${key}`}>
-        <a style={{ fontWeight: isActive ? 'bold' : 'normal' }} href={`${prefix}/${key}`}>{label}</a>
-
-        {typeof countryNavData === 'object' && isActive && renderDeepLinks(countryNavData)}
+        <a style={{ fontWeight: isActive ? 'bold' : 'normal' }} href={`${prefix}/${key}`}>{pages[key].label}</a>
+        {isActive && renderDeepLinks(nav.index[name][key])}
       </li>
     )
   }
@@ -95,12 +98,8 @@ const Aside = ({ nav, pages, pathArray }) => {
 
   return (
     <Fragment>
-      <ul>
-        {renderedLinks.slice(0, renderedLinks.length / 2)}
-      </ul>
-      <ul>
-        {renderedLinks.slice(renderedLinks.length / 2, renderedLinks.length)}
-      </ul>
+      <ul>{renderedLinks.slice(0, renderedLinks.length / 2)}</ul>
+      <ul>{renderedLinks.slice(renderedLinks.length / 2, renderedLinks.length)}</ul>
     </Fragment>
   )
 }
