b2407d74659c2fe385037cfbaaed8e6aa436cf06
Daniel Whiffing <dan@Daniels-MacBook-Pro.local>
2019-03-13 20:19:38 -0400

    Fixes google analytics & mailchimp

--- code/page.js
+++ code/page.js
@@ -53,10 +53,10 @@ const Page = ({
         )}
 
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136109754-1"></script>
-        <script>{`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)};gtag('js', new Date()); gtag('config', 'UA-136109754-1');`}</script>
+        <script type="text/javascript" dangerouslySetInnerHTML={{__html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)};gtag('js', new Date()); gtag('config', 'UA-136109754-1');`}} />
         {_ID === 'index' && <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script>}
-        {_ID === 'index' && <script>{`window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us20.list-manage.com","uuid":"40207b1c822d45c9de3c64ade","lid":"a5cb1201d5","uniqueMethods":true })})`}</script>}
-      </head>
+        {_ID === 'index' && <script type="text/javascript" dangerouslySetInnerHTML={{__html: `window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us20.list-manage.com","uuid":"40207b1c822d45c9de3c64ade","lid":"a5cb1201d5","uniqueMethods":true })})`}} />}
+      </head> 
 
       <body>
         <Header />
