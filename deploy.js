b7928addb8951fd95daa12bef2eafc7dab5db93b
Daniel Whiffing <dan@Daniels-MacBook-Pro.local>
2019-03-15 10:35:54 -0400

    Adds deploy script

---
+++ deploy.js
@@ -0,0 +1,23 @@
+var rsync = require('rsyncwrapper')
+
+require('dotenv').config()
+
+var USER = process.env.SSH_USER
+var DOMAIN = process.env.DOMAIN
+var FOLDER = process.env.FOLDER
+
+rsync(
+  {
+    src: 'fury/',
+    dest: `${USER}@${DOMAIN}:${FOLDER}`,
+    args: ['-a', '-v', '-u', '-z'],
+  },
+  function(error, stdout, stderr, cmd) {
+    console.log(`Running command: ${cmd}`)
+    if (error) {
+      console.log(`Encountered Error: ${error.message} (${stderr})`)
+    } else {
+      console.log(`Success: ${stdout}`)
+    }
+  }
+)
