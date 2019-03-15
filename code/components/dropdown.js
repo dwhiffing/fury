88b499e246f71369271eafa99709ee02618c5bad
Daniel Whiffing <dan@Daniels-MacBook-Pro.local>
2019-03-15 09:10:45 -0400

    Adds dropdown

---
+++ code/components/dropdown.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { PREFIX } from '../page'
+
+const WIDOW_COUNTRIES = ['Iceland', 'Luxembourg']
+const GROUP_LABELS = [['Air Force', 'air'], ['Army', 'army'], ['Navy', 'navy']]
+
+const Item = ({ label, href, children }) => (
+  <li>
+    <a href={href}>{label}</a>
+    {children}
+  </li>
+)
+
+const Dropdown = ({ tab, pathName }) => (
+  <ul className="dropdown">
+    {tab.items.map(({ label, path }) => {
+      const href = `${PREFIX}/${pathName}/${path}`
+
+      return (
+        <Item key={`inner-nav-${href}`} label={label} href={href}>
+          {!WIDOW_COUNTRIES.includes(label) && (
+            <ul className="dropdown">
+              {GROUP_LABELS.map(([groupLabel, groupPath]) => (
+                <Item
+                  key={`inner-nav-${href}-${groupLabel}`}
+                  label={groupLabel}
+                  href={`${href}/${groupPath}`}
+                />
+              ))}
+            </ul>
+          )}
+        </Item>
+      )
+    })}
+  </ul>
+)
+
+export default Dropdown
