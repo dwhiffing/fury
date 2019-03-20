e72f070f0e28308dbe9a8da81df3701b947cd42b
Daniel <danielwhiffing@gmail.com>
2019-03-20 10:30:48 -0400

    Fixes build on windows

--- build.js
+++ build.js
@@ -1,25 +1,9 @@
-const rimraf = require('rimraf')
-const mv = require('mv')
-const spawn = require('child_process').spawn
 const os = require('os')
 const platform = os.type() === 'Darwin' ? 'mac' : 'windows'
 const platformSlashes = platform === 'mac' ? '/' : '\\'
+const shell = require('shelljs')
 
-function exec(command, args, callback) {
-  console.log(`Running: "${command}"`)
-  const instance = spawn(command, args)
-  instance.stdout.on('data', data => console.log(data.toString()))
-  instance.stderr.on('data', data => console.log(data.toString()))
-  instance.on('exit', data => {
-    console.log(data.toString())
-    callback()
-  })
-}
-
-exec(`.${platformSlashes}doc-2-md.sh`, ['-lh', '/usr'], function() {
-  exec('cuttlebelle', {}, function() {
-    rimraf('html', {}, function() {
-      mv('site', 'html', function() {})
-    })
-  })
-})
+shell.exec(`.${platformSlashes}doc-2-md.sh`)
+shell.exec('cuttlebelle')
+shell.rm('-rf', 'html')
+shell.mv('site', 'html')
