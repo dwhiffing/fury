712fecdb11b4fa9ef8637e2c5b00df66f3301e36
Daniel Whiffing <danielwhiffing@gmail.com>
2021-07-23 14:11:10 -0400

    Updates nav to detect site content

--- code/components/dropdown.js
+++ code/components/dropdown.js
@@ -1,46 +1,29 @@
 import React from 'react'
 import { PREFIX } from '../page'
 
-const WIDOW_COUNTRIES = ['Iceland', 'Luxembourg']
-const GROUP_LABELS = [['Air Force', 'air'], ['Army', 'army'], ['Navy', 'navy']]
+const Dropdown = ({ tab, pathName }) => (
+  <ul className="dropdown">
+    {tab.items.map(({ label, url, subitems = [] }) => (
+      <Item key={`inner-nav-${url}`} label={label} href={url}>
+        <ul className="dropdown">
+          {subitems.map((subitem) => (
+            <Item
+              key={`inner-nav-${subitem.url}`}
+              label={subitem.label}
+              href={subitem.url}
+            />
+          ))}
+        </ul>
+      </Item>
+    ))}
+  </ul>
+)
 
 const Item = ({ label, href, children }) => (
   <li>
-    <a href={href}>{label}</a>
+    <a href={`${PREFIX}${href}`}>{label}</a>
     {children}
   </li>
-)
-
-const Dropdown = ({ tab, pathName }) => (
-  <ul className="dropdown">
-    {tab.items.map(({ label, path }) => {
-      const href = `${PREFIX}/${pathName}/${path}`
-
-      return (
-        <Item key={`inner-nav-${href}`} label={label} href={href}>
-          {!WIDOW_COUNTRIES.includes(label) && (
-            <ul className="dropdown">
-              {GROUP_LABELS.filter(groupLabel => {
-                if (path === 'cz' && groupLabel[1] === 'navy') {
-                  return false
-                }
-                if (path === 'hu' && groupLabel[1] === 'navy') {
-                  return false
-                }
-                return true
-              }).map(([groupLabel, groupPath]) => (
-                <Item
-                  key={`inner-nav-${href}-${groupLabel}`}
-                  label={groupLabel}
-                  href={`${href}/${groupPath}`}
-                />
-              ))}
-            </ul>
-          )}
-        </Item>
-      )
-    })}
-  </ul>
 )
 
 export default Dropdown
--- code/nav.js
+++ code/nav.js
@@ -8,38 +8,84 @@ const Logo = () => (
   </a>
 )
 
-const Nav = ({ _nav, tabs }) => (
-  <div className="nav-container">
-    <div className="mobile-container">
-      <Logo />
-    </div>
+const Nav = ({ _nav, _pages }) => {
+  const tabs = {}
+  Object.entries(_pages).forEach(([k, v]) => {
+    const keys = k.split('/')
+    if (keys.length === 1 && v._url !== '/') {
+      tabs[keys[0]] = { label: v.label || 'index', url: v._url, items: [] }
+    }
+  })
 
-    <ul>
-      <li className={`nav-top-button tab-home`}>
+  Object.entries(_pages).forEach(([k, v]) => {
+    const keys = k.split('/')
+    if (keys.length === 2 && tabs[keys[0]]) {
+      tabs[keys[0]].items = [
+        ...(tabs[keys[0]].items || []),
+        { label: v.label, url: v._url },
+      ]
+    }
+  })
+
+  Object.entries(_pages).forEach(([k, v]) => {
+    const keys = k.split('/')
+    const [key1, key2 = 'index', key3 = 'index'] = keys
+    if (keys.length === 3 && tabs[key1]) {
+      tabs[key1].items = (tabs[key1].items || []).map((item) => {
+        if (item.url.split('/')[2] === key2) {
+          return {
+            ...item,
+            subitems: [
+              ...(item.subitems || []),
+              { label: v.label, url: v._url },
+            ],
+          }
+        }
+        return item
+      })
+    }
+  })
+
+  return (
+    <div className="nav-container">
+      <div className="mobile-container">
         <Logo />
-      </li>
-
-      <div className="tab-container">
-        {Object.keys(tabs).map(tabKey => {
-          const tab = tabs[tabKey]
-          let path = _nav.index[tabKey] || ''
-          let pathName = typeof path === 'string' ? path : tabKey
-
-          return (
-            <li
-              key={`nav-${tabKey}`}
-              className={`nav-top-button tab-${pathName}`}>
-              <a href={`${PREFIX}/${pathName}`}>{tab.label}</a>
-
-              {typeof path === 'object' && (
-                <Dropdown tab={tab} pathName={pathName} />
-              )}
-            </li>
-          )
-        })}
       </div>
-    </ul>
-  </div>
-)
+
+      <ul>
+        <li className={`nav-top-button tab-home`}>
+          <Logo />
+        </li>
+
+        <div className="tab-container">
+          {Object.keys(tabs)
+            .sort((a, b) => {
+              if (a === 'overview') return -1
+              if (a === 'aar') return 1
+
+              return 0
+            })
+            .map((tabKey) => {
+              const tab = tabs[tabKey]
+              let path = _nav.index[tabKey] || ''
+              let pathName = typeof path === 'string' ? path : tabKey
+              return (
+                <li
+                  key={`nav-${tab.url}`}
+                  className={`nav-top-button tab-${pathName}`}
+                >
+                  <a href={`${PREFIX}/${pathName}`}>{tab.label}</a>
+
+                  {typeof path === 'object' && (
+                    <Dropdown tab={tab} pathName={pathName} />
+                  )}
+                </li>
+              )
+            })}
+        </div>
+      </ul>
+    </div>
+  )
+}
 
 export default Nav
